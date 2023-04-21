4b2b7b3d9acca285b1e3887c2319d99971865ccc
unknown <paramjoshi27@gmail.com>
2023-04-22 03:33:53 +0530

    trying to solve production error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import staticAssets from 'vite-plugin-static-assets';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    staticAssets({
+      include: ['**/*.png'], // replace with the file extensions of your image assets
+      outputDir: 'assets', // replace with the directory where you want to output the assets
+    }),
+  ],
+});
